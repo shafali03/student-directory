27fbbb9ca59024fc8b75f6b8fdf09c401cbf0af6
shafali03 <shaf03sm@gmail.com>
2019-11-06 09:38:10 +0000

    file update

--- directory_2.rb
+++ directory_2.rb
@@ -1,65 +1,65 @@
-@students = [] # an empty array accessible to all methods
-
 def input_students
-  puts "Please enter the names of students"
+  puts "Please enter the names of the students and cohort"
   puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
   # get the first name
-  name = gets.chomp
+  puts "Enter name"
+  name = gets.strip
+  if name.empty?
+    puts "No value recorded for name"
+  end
+
+  puts "Enter cohort"
+  cohort = gets.strip
+  if cohort.empty?
+    puts "No value recorded for cohort"
+  end
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
+    students << {name: name, cohort: cohort}
+    puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
-end
-end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
+    puts "Enter name"
+    name = gets.strip
+    if name.empty?
+      puts "No value recorded for name"
+    end
+
+    puts "Enter cohort"
+    cohort = gets.strip
+    if cohort.empty?
+      puts "No value recorded for cohort"
+    end
   end
-end
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit" # 9 because we'll bee adding more items
-end
-
-def show_students
-  print_header
-  print_student_list
-  print_footer
-end
-
-def process(selection)
-  case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "9"
-    exit # this will cause the program to terminate
-  else
-    puts "I don't know what you meant, try again"
-  end
+  # return the array of students
+  students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(30)
+  puts "-------------".center(30)
 end
 
-def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index + 1} #{student[:name]} #{student[:cohort]}".center(30)
   end
 end
 
-def print_footer
-    puts "Overall, we have #{@students.count} great students"
+def print_footer(students)
+  if students.count == 0
+    puts "No record, we have #{students.count} student"
+  elsif students.count < 2
+    puts "Overall, we have #{students.count} great student".center(30)
+  elsif students.count >= 2
+    puts "Overall, we have #{students.count} great students".center(30)
+  end
 end
-
-interactive_menu
+#nothing happens until we call the method
+students = input_students
+print_header
+print(students)
+print_footer(students)
