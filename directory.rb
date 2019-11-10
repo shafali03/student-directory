33a5c655723b8c5a1badd65e5ec183cf78e3f939
shafali03 <shaf03sm@gmail.com>
2019-11-10 14:03:15 +0000

    refactor

--- directory.rb
+++ directory.rb
@@ -5,13 +5,13 @@ def print_menu
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
-  puts "9. Exit"
+  puts "9. Exit" # 9 because we'll be adding more items
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(STDIN.gets.chomp)
+    process(STDIN.gets.strip)
   end
 end
 
@@ -21,10 +21,14 @@ def process(selection)
     input_students
   when "2"
     show_students
+     puts "Show student successful"
   when "3"
-    save_students
+
+     puts "Saved student successful"
+     save_student
   when "4"
     load_students
+     puts "Loading student successful"
   when "9"
     exit # this will cause the program to terminate
   else
@@ -33,14 +37,13 @@ def process(selection)
 end
 
 def input_students
-
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-
+  # get the first name
   puts "Enter name"
   name = STDIN.gets.strip
   if name.empty?
-    puts "No value recorded for name"
+    puts "No value recorder for name"
   end
 
   puts "Enter cohort"
@@ -49,22 +52,29 @@ def input_students
     puts "No value recorded for cohort"
   end
 
+  # while the name is not empty, repeat this code
   while !name.empty? do
+    # add the student hash to the array
     @students << {name: name, cohort: cohort}
-    puts "Now we have #{@students.count} students"
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
+    else
+      puts "Now we have #{@students.count} students"
+    end
+    # get another name from the user
+
     puts "Enter name"
     name = STDIN.gets.strip
     if name.empty?
-      puts "No value recorded for name"
+      puts "No value recorded"
     end
 
     puts "Enter cohort"
-    cohort = gets.empty?
-    if cohort = STDIN.gets.strip
-      puts "No value recorded for cohort"
+    cohort = STDIN.gets.strip
+    if cohort.empty?
+      puts "No value recorded"
     end
   end
-
   @students
 end
 
@@ -73,6 +83,7 @@ def show_students
   print_student_list
   print_footer
 end
+
 
 def save_students
   # open the file for writing
@@ -86,25 +97,25 @@ def save_students
   file.close
 end
 
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.strip.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def try_load_students
   filename = ARGV.first# first argument from the command line
   return if filename.nil? # get out of the method if it isn't given
   if File.exists?(filename) # if it exists
     load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
+    puts "Loaded #{@students.count} from #{filename}".center(30)
   else # if it doesn't exist
-    puts "Sorry, #{filename} doesn't exist."
+    puts "Sorry, #{filename} doesn't exist.".center(30)
     exit # quit the program
   end
-end
-
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-    name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
 end
 
 def print_header
@@ -113,21 +124,17 @@ def print_header
 end
 
 def print_student_list
-  @students.each_with_index do |student, index|
-    puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)".center(30)
+  @students.each_with_index { |student, index|
+    puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)" }
   end
-end
 
-def print_footer
-  # puts "Overall, we have #{@students.count} great students"
-  if @students.count == 0
-    puts "No record, we have #{@students.count} student"
-  elsif @students.count < 2
-    puts "Overall, we have #{@students.count} great student".center(30)
-  elsif @students.count >= 2
-    puts "Overall, we have #{@students.count} great students".center(30)
+  def print_footer
+    if @students.count == 1
+      puts "Overall, we have #{@students.count} great student".center(30)
+    else
+      puts "Overall, we have #{@students.count} great students".center(30)
+    end
   end
-end
 
-try_load_students
-interactive_menu
+  try_load_students
+  interactive_menu
